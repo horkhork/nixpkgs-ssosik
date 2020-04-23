48e17ed855e01e3004b74bca39e1c64688bbf9e6
Your Name <you@example.com>
2020-04-23 13:27:00 -0400

    Trying to get adding <nixpkgs-ssosik> to configuration.nix to work
    Attempting to fix this error:
    error: The option `dnscrypt-proxy2-blacklist-updater' defined in
    `/nix/var/nix/profiles/per-user/root/channels/nixpkgs-ssosik' does not
    exist.
    (use '--show-trace' to show detailed location information)
    building Nix...

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ let
   callPackage = pkgs.lib.callPackageWith (pkgs // self);
 
   self = {
-    dnscrypt-proxy2-blacklist-updater = callPackage ./dnscrypt-proxy2-blacklist-updater.nix { };
+    pkgs.dnscrypt-proxy2-blacklist-updater = callPackage ./dnscrypt-proxy2-blacklist-updater.nix { };
   };
 in
 self
