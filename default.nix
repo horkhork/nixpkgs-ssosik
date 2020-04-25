4fbc37892992d07c3fc1803897954f050721c283
Your Name <you@example.com>
2020-04-24 21:26:23 -0400

    Following example in https://blog.hackeriet.no/packaging-python-script-for-nixos/

--- default.nix
+++ default.nix
@@ -1,12 +1,53 @@
-{ system ? builtins.currentSystem, pkgs ? import <nixpkgs> {}, ... }:
+# Below, we can supply defaults for the function arguments to make the script
+# runnable with `nix-build` without having to supply arguments manually.
+# Also, this lets me build with Python 3.7 by default, but makes it easy
+# to change the python version for customised builds (e.g. testing).
+{ nixpkgs ? import <nixpkgs> {}, pythonPkgs ? nixpkgs.pkgs.python37Packages }:
 
 let
-  pkgs = import <nixpkgs> { inherit system; };
+  # This takes all Nix packages into this scope
+  inherit (nixpkgs) pkgs;
+  # This takes all Python packages from the selected version into this scope.
+  inherit pythonPkgs;
 
-  callPackage = pkgs.lib.callPackageWith (pkgs // self);
+  # Inject dependencies into the build function
+  f = { buildPythonPackage, bottle, requests }:
+    buildPythonPackage rec {
+      pname = "dnscrypt-proxy2-blacklist-updater";
+      version = "1.0";
 
-  self = {
-    pkgs.dnscrypt-proxy2-blacklist-updater = callPackage ./dnscrypt-proxy2-blacklist-updater.nix { };
-  };
+      src = builtins.fetchFromGitHub {
+        owner = "jedisct1";
+        repo = "dnscrypt-proxy";
+        rev = version;
+        sha256 = "1v4n0pkwcilxm4mnj4fsd4gf8pficjj40jnmfkiwl7ngznjxwkyw";
+      };
+
+      buildPhase = ''
+        # Skip build
+      '';
+      checkPhase = ''
+        # Skip check
+      '';
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp $src/utils/generate-domains-blacklists/generate-domains-blacklist.py $out/bin/generate-domains-blacklist.py
+        cp $src/utils/generate-domains-blacklists/domains-blacklist.conf $out/domains-blacklist.conf
+        cp $src/utils/generate-domains-blacklists/domains-time-restricted.txt $out/domains-time-restricted.txt
+        cp $src/utils/generate-domains-blacklists/domains-whitelist.txt $out/domains-whitelist.txt
+      '';
+
+      meta = {
+        description = "";
+        #license = licenses.isc;
+        homepage = "";
+        #maintainers = with maintainers; [ ssosik ];
+        #platforms = with platforms; unix;
+      };
+    };
+
+  drv = pythonPkgs.callPackage f {};
 in
-self
+  if pkgs.lib.inNixShell then drv.env else drv
+
--- dnscrypt-proxy2-blacklist-updater.nix
+++ dnscrypt-proxy2-blacklist-updater.nix
@@ -1,44 +1,52 @@
-{ stdenv, python3Packages, fetchFromGitHub }:
+ {config, lib, pkgs, ...}:
+ 
+ let
+   updater = pkgs.callPackage ./default.nix {};
 
-# TODOs
-#  - get dnscrypt-proxy2-blacklist-updater into git somewhere and pull it in
-#  - Add crontab entry here with options
-#  - Based on options generate domains-blacklist.conf, domains-whitelist, time-restricted, custom
-#  - actually hook up to dnscrypt-proxy2
-#  - run cronjob as non-root
-#  - instead of cron, run the updater as a systemd unit
+   cfg = config.services.dnscrypt-proxy2-blacklist-updater;
+ in
+ 
+ {
+   options = {
+     services.dnscrypt-proxy2-blacklist-updater = {
+       enable = lib.mkOption {
+         default = false;
+         type = with lib.types; bool;
+         description = ''
+           Enable a blacklist updater for dnscrypt-proxy2
+         '';
+       };
+ 
+       user = lib.mkOption {
+         default = "root";
+         type = with lib.types; uniq string;
+         description = ''
+           Name of the user.
+         '';
+       };
+     };
+   };
+ 
+   config = lib.mkIf cfg.enable {
+     #systemd.services.ircSession = {
+     #  wantedBy = [ "multi-user.target" ]; 
+     #  after = [ "network.target" ];
+     #  description = "Start the irc client of username.";
+     #  serviceConfig = {
+     #    Type = "forking";
+     #    User = "${cfg.user}";
+     #    ExecStart = ''${pkgs.screen}/bin/screen -dmS irc ${pkgs.irssi}/bin/irssi'';         
+     #    ExecStop = ''${pkgs.screen}/bin/screen -S irc -X quit'';
+     #  };
+     #};
 
-python3Packages.buildPythonApplication rec {
-  pname = "dnscrypt-proxy2-blacklist-updater";
-  version = "1.0";
-
-  src = fetchFromGitHub {
-    owner = "jedisct1";
-    repo = "dnscrypt-proxy";
-    rev = version;
-    sha256 = "1v4n0pkwcilxm4mnj4fsd4gf8pficjj40jnmfkiwl7ngznjxwkyw";
-  };
-
-  buildPhase = ''
-    # Skip build
-  '';
-  checkPhase = ''
-    # Skip check
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp $src/utils/generate-domains-blacklists/generate-domains-blacklist.py $out/bin/generate-domains-blacklist.py
-    cp $src/utils/generate-domains-blacklists/domains-blacklist.conf $out/domains-blacklist.conf
-    cp $src/utils/generate-domains-blacklists/domains-time-restricted.txt $out/domains-time-restricted.txt
-    cp $src/utils/generate-domains-blacklists/domains-whitelist.txt $out/domains-whitelist.txt
-  '';
-
-  meta = with stdenv.lib; {
-    description = "A tool that can be added to crontab for automatically updating dnscrypt-proxy2 blacklists";
-    license = licenses.isc;
-    homepage = "";
-    maintainers = with maintainers; [ "ssosik" ];
-    platforms = with platforms; unix;
-  };
-}
+     services.cron = {
+       enable = true;
+       systemCronJobs = [
+         "0 0 * * *      ${cfg.user}    ${updater}/bin/generate-domains-blacklist.py -i -c ${updater}/domains-blacklist.conf -r ${updater}/domains-time-restricted.txt -w ${updater}/domains-whitelist.txt > /var/lib/dnscrypt-proxy2/dnscrypt-proxy-blacklist.txt"
+       ];
+     };
+ 
+     #environment.systemPackages = [ pkgs.dnscrypt-proxy2 pkgs.dnscrypt-proxy2-blacklist-updater ];
+   };
+ }
